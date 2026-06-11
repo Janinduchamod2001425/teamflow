86defec1983f33a52eca29fe2b8b3f6e98fb1d8c
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-11 20:51:39 +0530

    Update users.controller.ts

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Get all users',
+  })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Create a new user',
+  })
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
