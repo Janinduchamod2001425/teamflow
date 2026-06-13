b13ecbb83f961c60c80c493d5af64590dbd13f9d
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-13 10:02:50 +0530

    Update auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { ResponseMessage } from '../common/decorators/response-message.decorator';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ResponseMessage('User registered successfully')
+  @ApiOperation({ summary: 'Register a new user' })
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @ResponseMessage('User logged in successfully')
+  @ApiOperation({ summary: 'Login a user' })
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
