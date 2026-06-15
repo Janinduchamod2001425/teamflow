a3ce6831bb64fb63136c5a9d44dfcba1b353eef1
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-15 10:58:21 +0530

    Update app.module.ts
    create new module as workspace access for create role base access

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { WorkspaceMembersModule } from './workspace-members/workspace-members.module';
+import { WorkspaceAccessModule } from './workspace-access/workspace-access.module';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { WorkspacesModule } from './workspaces/workspaces.module';
     UsersModule,
     AuthModule,
     WorkspacesModule,
+    WorkspaceMembersModule,
+    WorkspaceAccessModule,
   ],
   controllers: [],
   providers: [
