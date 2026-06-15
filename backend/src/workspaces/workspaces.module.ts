b76b64b41c9f2dd5086056de82b1eb521800ea7a
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-15 11:05:56 +0530

    Update workspaces.module.ts

--- backend/src/workspaces/workspaces.module.ts
+++ backend/src/workspaces/workspaces.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesController } from './workspaces.controller';
 import { WorkspacesService } from './workspaces.service';
+import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
 
 @Module({
+  imports: [WorkspaceAccessModule],
   controllers: [WorkspacesController],
-  providers: [WorkspacesService]
+  providers: [WorkspacesService],
 })
 export class WorkspacesModule {}
