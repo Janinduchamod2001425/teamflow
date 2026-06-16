75691e7f5a9d6d1bf8aad1f078ffa251e2ecdadf
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-16 19:08:50 +0530

    Update comments.module.ts

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
 import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { ActivitiesModule } from '../activities/activities.module';
 
 @Module({
-  imports: [WorkspaceAccessModule],
+  imports: [WorkspaceAccessModule, ActivitiesModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
