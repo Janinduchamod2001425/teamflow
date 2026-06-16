37b17a60e022f692e4a9041bf7fff852b47bccf6
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-16 19:04:37 +0530

    Update activities.module.ts

--- backend/src/activities/activities.module.ts
+++ backend/src/activities/activities.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
 import { ActivitiesController } from './activities.controller';
 import { ActivitiesService } from './activities.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [WorkspaceAccessModule],
+  imports: [WorkspaceAccessModule, NotificationsModule],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
   exports: [ActivitiesService],
