6a4bfcb2990ae9cf55eba2e0a69e8ce0610faa94
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-17 17:13:04 +0530

    Update attachments.module.ts

--- backend/src/attachments/attachments.module.ts
+++ backend/src/attachments/attachments.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AttachmentsController } from './attachments.controller';
 import { AttachmentsService } from './attachments.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
 
 @Module({
+  imports: [MulterModule.register(), WorkspaceAccessModule],
   controllers: [AttachmentsController],
-  providers: [AttachmentsService]
+  providers: [AttachmentsService],
 })
 export class AttachmentsModule {}
