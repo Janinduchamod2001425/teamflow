098d3724027abdbbe456a20fb279b7f197a06778
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-17 15:31:27 +0530

    Update workspace-members.module.ts

--- backend/src/workspace-members/workspace-members.module.ts
+++ backend/src/workspace-members/workspace-members.module.ts
@@ -3,9 +3,10 @@ import { WorkspaceMembersController } from './workspace-members.controller';
 import { WorkspaceMembersService } from './workspace-members.service';
 import { UsersModule } from '../users/users.module';
 import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [UsersModule, WorkspaceAccessModule],
+  imports: [UsersModule, WorkspaceAccessModule, NotificationsModule],
   controllers: [WorkspaceMembersController],
   providers: [WorkspaceMembersService],
 })
