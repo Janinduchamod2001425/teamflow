d4347ff9bdd55e68dc4f20642e7d95cc07c96187
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-17 15:05:24 +0530

    Update notifications.module.ts

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsGateway } from './notifications.gateway';
+import { NotificationsController } from './notifications.controller';
 
 @Module({
   providers: [NotificationsService, NotificationsGateway],
   exports: [NotificationsService],
+  controllers: [NotificationsController],
 })
 export class NotificationsModule {}
