7812a7dda2c05ac4cea13a95c72e1df9f359cef6
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-17 15:31:08 +0530

    Update tasks.module.ts

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { WorkspaceAccessModule } from '../workspace-access/workspace-access.module';
 import { ActivitiesModule } from '../activities/activities.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [WorkspaceAccessModule, ActivitiesModule],
+  imports: [WorkspaceAccessModule, ActivitiesModule, NotificationsModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
