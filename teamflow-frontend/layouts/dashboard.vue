49413309e89659e0b138fefddb655359a4bd180a
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-18 18:45:15 +0530

    Create dashboard.vue

---
+++ teamflow-frontend/layouts/dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-slate-50">
+    <Sidebar />
+
+    <div class="ml-64 min-h-screen">
+      <Topbar />
+
+      <PageContainer>
+        <slot />
+      </PageContainer>
+    </div>
+  </div>
+</template>
