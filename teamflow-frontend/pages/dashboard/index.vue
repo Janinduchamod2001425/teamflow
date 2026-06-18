434065a80750743aec7e1eb1c81c4092406f12a1
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-18 18:46:04 +0530

    Create index.vue

---
+++ teamflow-frontend/pages/dashboard/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1 class="text-2xl font-bold text-slate-900">
+      Welcome to TeamFlow Dashboard 🚀
+    </h1>
+
+    <p class="mt-2 text-slate-600">Dashboard layout is working correctly.</p>
+  </section>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "dashboard",
+});
+</script>
