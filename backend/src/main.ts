e77b6c8fe0dfcf358151e437d134ca70dbeaf0ad
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-18 18:43:55 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,12 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    credentials: true,
+  });
+
   const reflector = app.get(Reflector);
   app.useGlobalInterceptors(new ResponseInterceptor(reflector));
 
