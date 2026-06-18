f2d786424bb02e5f4a5347c0582a0f2ea58aee4a
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-06-18 18:44:30 +0530

    Create Logo.vue

---
+++ teamflow-frontend/components/layout/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink class="flex items-center gap-2" to="/">
+    <div
+      class="flex h-10 w-10 items-center justify-center rounded-xl bg-indigo-600 text-white"
+    >
+      TF
+    </div>
+
+    <div>
+      <h1 class="text-lg font-bold text-slate-900">TeamFlow</h1>
+      <p class="text-xs text-slate-500">Collaboration App</p>
+    </div>
+  </NuxtLink>
+</template>
