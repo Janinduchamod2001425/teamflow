6fe7abe98f53290fe95d5e906824cddeb674dd2b
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-10 11:29:52 +0530

    Create DashboardStats.vue

---
+++ teamflow-frontend/components/dashboard/DashboardStats.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
+    <DashboardCard :value="12" title="Workspaces" />
+
+    <DashboardCard :value="34" title="Projects" />
+
+    <DashboardCard :value="185" title="Tasks" />
+
+    <DashboardCard :value="142" title="Completed" />
+  </div>
+</template>
