9384c6aaac82ad0a64debd4af9e2d2af6b6e340c
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 09:28:28 +0530

    Create StatCard.vue

---
+++ teamflow-frontend/components/dashboard/StatCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="rounded-2xl border border-slate-200 bg-white p-6 shadow-sm transition hover:shadow-md"
+  >
+    <div class="text-sm text-slate-500">
+      {{ title }}
+    </div>
+
+    <div class="mt-3 text-4xl font-bold">
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  value: number;
+}>();
+</script>
