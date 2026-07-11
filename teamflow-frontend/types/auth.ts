73175add2ad9a79ffe701be928a16a99e3c282ab
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 09:30:15 +0530

    Create types\auth.ts

---
+++ teamflow-frontend/types/auth.ts
@@ -0,0 +1,18 @@
+import type { User } from "./user";
+
+export interface LoginDto {
+  email: string;
+  password: string;
+}
+
+export interface RegisterDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: User;
+}
