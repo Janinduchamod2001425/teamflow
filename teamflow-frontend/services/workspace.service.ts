7ecc63e3535b967b3f2de630738c43c07787a268
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 09:29:56 +0530

    Update services\workspace.service.ts

--- teamflow-frontend/services/workspace.service.ts
+++ teamflow-frontend/services/workspace.service.ts
@@ -4,4 +4,16 @@ export const WorkspaceService = {
   findAll() {
     return api.get("/workspaces");
   },
+
+  create(data: any) {
+    return api.post("/workspaces", data);
+  },
+
+  update(id: string, data: any) {
+    return api.patch(`/workspaces/${id}`, data);
+  },
+
+  remove(id: string) {
+    return api.delete(`/workspaces/${id}`);
+  },
 };
