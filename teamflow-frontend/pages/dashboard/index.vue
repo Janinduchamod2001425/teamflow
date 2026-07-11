b0b0bf638bf8d71639353d90c17c1c5304aac332
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 09:29:35 +0530

    Update index.vue

--- teamflow-frontend/pages/dashboard/index.vue
+++ teamflow-frontend/pages/dashboard/index.vue
@@ -1,16 +1,30 @@
 <template>
-  <section>
-    <h1 class="text-2xl font-bold text-slate-900">
-      Welcome to TeamFlow Dashboard 🚀
-    </h1>
+  <section class="space-y-6">
+    <div>
+      <h1 class="text-3xl font-bold">
+        {{ workspace?.name }}
+      </h1>
 
-    <p class="mt-2 text-slate-600">Dashboard layout is working correctly.</p>
+      <p class="text-slate-500">
+        {{ workspace?.description }}
+      </p>
+    </div>
   </section>
 </template>
 
 <script lang="ts" setup>
+import StatCard from "~/components/dashboard/StatCard.vue";
+
 definePageMeta({
   layout: "dashboard",
   middleware: "auth",
+});
+
+const workspaceStore = useWorkspaceStore();
+
+const workspace = computed(() => workspaceStore.selectedWorkspace);
+
+onMounted(() => {
+  workspaceStore.initializeWorkspace();
 });
 </script>
