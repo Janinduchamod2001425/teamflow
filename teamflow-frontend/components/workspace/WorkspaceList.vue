6bddd2cf71c87bad1b8b284f4438da62cd13edc4
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 09:28:51 +0530

    Create WorkspaceList.vue

---
+++ teamflow-frontend/components/workspace/WorkspaceList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+    <WorkspaceCard
+      v-for="workspace in workspaces"
+      :key="workspace.id"
+      :workspace="workspace"
+      @delete="$emit('delete', $event)"
+      @edit="$emit('edit', $event)"
+      @open="$emit('open', $event)"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import WorkspaceCard from "./WorkspaceCard.vue";
+
+import type { Workspace } from "~/types/workspace";
+
+defineProps<{
+  workspaces: Workspace[];
+}>();
+
+defineEmits(["open", "edit", "delete"]);
+</script>
