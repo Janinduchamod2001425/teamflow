12fb1c6b347be2fb48f0ebbddadaccca6afc4e50
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-07-11 16:00:59 +0530

    Update app.vue

--- teamflow-frontend/app.vue
+++ teamflow-frontend/app.vue
@@ -2,4 +2,8 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  <ToastContainer />
 </template>
+<script lang="ts" setup>
+import ToastContainer from "~/components/ui/ToastContainer.vue";
+</script>
